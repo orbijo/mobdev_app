4c76908abda5357ac0c165ada2fe94f25187aa33
SenpaiKames <james_banes17@yahoo.com>
2022-11-10 18:24:47 +0800

    changed font style and color

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,14 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.brown,
       scaffoldBackgroundColor: Color.fromARGB(255, 66, 133, 91),
+      textTheme: 
+        TextTheme (
+          displaySmall: TextStyle(color: Colors.white),    
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+        filled: true, 
+        fillColor: Colors.white,  
+        ), 
     ),
     debugShowCheckedModeBanner: false,
     initialRoute: LoginScreen.routeName,
