86c1f978db8f78f9b63efcbea95fa47ae202422a
Josh Arkane Orbiso <orbisojo@gmail.com>
2022-11-10 18:25:59 +0800

    dashboard changes

--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:mobdev_app/screens/Settings.dart';
 import 'package:mobdev_app/arguments/FormData.dart';
+import 'package:flutter/services.dart';
 
 class Dashboard extends StatefulWidget {
   static String routeName = "/dashboard";
@@ -10,12 +12,23 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-  
+  static const MethodChannel _methodChannel =
+      MethodChannel('dev.flutter.sample/platform_view_swift');
+
+  int _counter = 0;
+
+  Future<void> _launchPlatformCount() async {
+    final platformCounter =
+        await _methodChannel.invokeMethod<int>('switchView', _counter);
+    setState(() => _counter = platformCounter ?? 0);
+  }
+
   @override
   Widget build(BuildContext context) {
     var formdata = ModalRoute.of(context)!.settings.arguments as FormData;
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Dashboard'),
         actions: [
           GestureDetector(
             onTap: () {
@@ -31,21 +44,90 @@ class _DashboardState extends State<Dashboard> {
           )
         ],
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                  Text("Welcome, \n",
-                    style: Theme.of(context).textTheme.displaySmall,
-                    textAlign: TextAlign.center,
-                   ),
-                   Text(formdata.email)
-               ]
-               
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            formdata.email,
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Button tapped $_counter time${_counter == 1 ? '' : 's'}.',
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  const SizedBox(height: 18),
+                  ElevatedButton(
+                    onPressed: _launchPlatformCount,
+                    child: const Text('Continue in iOS view'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(bottom: 15, left: 5),
+            child: Row(
+              children: [
+                Image.asset('images/starbs2.png', height: 50, width: 50),
+                SizedBox(
+                  width: 8.0,
                 ),
-
-
-            );
-       
+                Text(
+                  'Starbucks',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => _counter++),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     actions: [
+    //       GestureDetector(
+    //         onTap: () {
+    //           Navigator.pushNamed(context, Settings.routeName);
+    //         },
+    //         child: const Padding(
+    //           padding: EdgeInsets.only(right: 15.0),
+    //           child: Icon(
+    //             Icons.settings,
+    //             size: 30,
+    //           ),
+    //         ),
+    //       )
+    //     ],
+    //   ),
+    //   // body: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+    //   //   Text(
+    //   //     "Welcome",
+    //   //     style: Theme.of(context).textTheme.displaySmall,
+    //   //     textAlign: TextAlign.center,
+    //   //   ),
+    //   //   Text(
+    //   //     formdata.email,
+    //   //     style: Theme.of(context).textTheme.displaySmall,
+    //   //     textAlign: TextAlign.center,
+    //   //   )
+    //   // ]),
+    //   body: Center(
+    //     child: Row(
+    //       children: [
+    //         Center()
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
-
 }
--- lib/screens/Signup.dart
+++ lib/screens/Signup.dart
@@ -25,80 +25,78 @@ class _SignupState extends State<Signup> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Center(
-              child: Container(
-                width: width * .9,
-                child: Column(
-                  children: [
-                    CustomTextField(
-                      labelText: "First Name",
-                      hintText: "Enter your first name",
-                      controller: firstNameController,
-                      textInputType: TextInputType.name,
-                    ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    CustomTextField(
-                      labelText: "Last Name",
-                      hintText: "Enter your last name",
-                      controller: lastNameController,
-                      textInputType: TextInputType.name,
-                    ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    CustomTextField(
-                      labelText: "Email Address",
-                      hintText: "Enter your email address",
-                      controller: emailController,
-                      textInputType: TextInputType.emailAddress,
-                    ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    PasswordField(
-                      obscureText: obscurePassword,
-                      onTap: handleObscurePassword,
-                      labelText: "Password",
-                      hintText: "Enter your password",
-                      controller: passwordController,
-                    ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    PasswordField(
-                      obscureText: obscurePassword,
-                      onTap: handleObscurePassword,
-                      labelText: "Confirm Password",
-                      hintText: "Re-enter your password",
-                      controller: confirmPassController,
-                    ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    PrimaryButton(
-                      text: "Signup",
-                      iconData: Icons.person_add,
-                      onPress: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    PrimaryButton(
-                      text: "Already have an account?",
-                      iconData: Icons.login,
-                      onPress: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ],
-                ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Center(
+            child: SizedBox(
+              width: width * .9,
+              child: Column(
+                children: [
+                  CustomTextField(
+                    labelText: "First Name",
+                    hintText: "Enter your first name",
+                    controller: firstNameController,
+                    textInputType: TextInputType.name,
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  CustomTextField(
+                    labelText: "Last Name",
+                    hintText: "Enter your last name",
+                    controller: lastNameController,
+                    textInputType: TextInputType.name,
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  CustomTextField(
+                    labelText: "Email Address",
+                    hintText: "Enter your email address",
+                    controller: emailController,
+                    textInputType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  PasswordField(
+                    obscureText: obscurePassword,
+                    onTap: handleObscurePassword,
+                    labelText: "Password",
+                    hintText: "Enter your password",
+                    controller: passwordController,
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  PasswordField(
+                    obscureText: obscurePassword,
+                    onTap: handleObscurePassword,
+                    labelText: "Confirm Password",
+                    hintText: "Re-enter your password",
+                    controller: confirmPassController,
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  PrimaryButton(
+                    text: "Signup",
+                    iconData: Icons.person_add,
+                    onPress: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  PrimaryButton(
+                    text: "Already have an account?",
+                    iconData: Icons.login,
+                    onPress: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
               ),
             ),
           ),
